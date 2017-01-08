70fd91bdb9c0a0010e2850cf0b7b76b64cb84124
GongUi <zhanggoni@gmail.com>
2017-01-08 16:04:03 +0900

    Update main.cpp

--- C++/1002/main.cpp
+++ C++/1002/main.cpp
@@ -11,16 +11,16 @@ int main(void){
     while (t--) {
         int x1, y1, r1, x2, y2, r2;
         scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
-        double distance = sqrt(((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)));
+        double d = sqrt(((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)));
         double rSum = r1 + r2;
-        double rDiffer = abs(r1 - r2);
+        double rDif = abs(r1 - r2);
         int ret = 0;
         
         if (x1 == x2 && y1 == y2 && r1 == r2) {
             ret = -1;
-        } else if (rDiffer == distance || rSum == distance) {
+        } else if (rDif == d || rSum == d) {
             ret = 1;
-        } else if (rDiffer < distance && distance < rSum) {
+        } else if (rDif < d && d < rSum) {
             ret = 2;
         }
         
