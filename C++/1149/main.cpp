822aed2a8c9652ddc8e862a402ccfa01a5b67cbe
GongUi <zhanggoni@gmail.com>
2017-01-24 22:46:50 +0900

    rgb거리 조금 더 최적화

--- C++/1149/main.cpp
+++ C++/1149/main.cpp
@@ -4,53 +4,50 @@
 #include <cstring>
 #include <cmath>
 #include <queue>
-#define MAX 1001
 using namespace std;
 
-int a[MAX][3];
-int d[MAX][3];
+int a[3];
+int d[3];
+int k[3];
 
 int rgb(int n, int idx);
 
 int main(void) {
     memset(a, 0, sizeof(a));
-    memset(d, -1, sizeof(d));
+    memset(d, 0, sizeof(d));
     
     int n;
     scanf("%d", &n);
-    for(int i = 0; i < n; i++) {
+    
+    for(int i = 0; i < 3; i++) {
+        scanf("%d", &a[i]);
+        d[i] = a[i];
+    }
+    
+    for(int i = 1; i < n; i++) {
         for(int j = 0; j < 3; j++) {
-            scanf("%d", &a[i][j]);
+            
+            scanf("%d", &a[j]);
+            int idx1 = j-1;
+            int idx2 = j+1;
+            
+            if(idx1 == -1) {
+                idx1 = 2;
+            }
+            if(idx2 == 3) {
+                idx2 = 0;
+            }
+            
+            k[j] = a[j] + min(d[idx1], d[idx2]);
+        }
+        for(int j = 0; j < 3; j++) {
+            d[j] = k[j];
         }
     }
-    
+ 
     int ans = ~(1<<31);
     for(int i = 0; i < 3; i++) {
-        ans = min(ans, rgb(n-1, i));
+        ans = min(ans, d[i]);
     }
-    
     printf("%d\n", ans);
-}
-
-int rgb(int n, int idx) {
-    if(n < 0) {
-        return 0;
-    }
-    
-    if (idx < 0) {
-        idx = 2;
-    }
-    if (idx > 2) {
-        idx = 0;
-    }
-    
-    int& cache = d[n][idx];
-    
-    if(n == 0) {
-        return cache = a[0][idx];
-    }
-    if(cache > -1) {
-        return cache;
-    }
-    return cache = a[n][idx] + min(rgb(n-1, idx-1), rgb(n-1, idx+1));
 }
