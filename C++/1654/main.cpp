7b8f587480f64697b2d10fd071b53e2f5c49de43
gonini <zhanggoni@gmail.com>
2017-02-09 23:24:54 +0900

    1654

---
+++ C++/1654/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#define MAX 14
+
+using namespace std;
+int cache[MAX];
+
+
+int main(void) {
+    // insert code here...
+    memset(cache, 0, sizeof(cache));
+
+}
